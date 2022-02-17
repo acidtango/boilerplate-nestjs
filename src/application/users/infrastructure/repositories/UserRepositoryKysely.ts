b49873a65725cc70d33b13bc5fc6b69adb7b59b4
Daniel Ramos <danielramosacosta1@gmail.com>
2022-02-17 22:12:49 +0000

    refactor: UserRepositoryKysely

--- src/application/users/infrastructure/repositories/UserRepositoryKysely.ts
+++ src/application/users/infrastructure/repositories/UserRepositoryKysely.ts
@@ -1,5 +1,6 @@
+import { sql } from 'kysely'
 import { UserRepository } from '../../domain/UserRepository'
-import { User } from '../../domain/User'
+import { User, UserPrimitives } from '../../domain/User'
 import { UserId } from '../../../../shared/domain/ids/UserId'
 import { UserPhoneNumber } from '../../domain/UserPhoneNumber'
 import { Nullable } from '../../../../shared/domain/utils/Nullable'
@@ -9,31 +10,78 @@ import { ContactsTable } from '../../../../database/schemas/ContactsTable'
 import { UsersTable } from '../../../../database/schemas/UsersTable'
 import { DATABASE_CONNECTION, DatabaseConnection } from '../../../../database/DatabaseConnection'
 
+type UserTableAggreagation = UsersTable & { contacts: ContactsTable[] }
+
 export class UserRepositoryKysely implements UserRepository {
+  private static toDomain = (user: UserTableAggreagation) =>
+    User.fromPrimitives({
+      id: user.id,
+      phone: user.phone,
+      name: user.phone,
+      lastName: user.last_name,
+      contacts: user.contacts.map(
+        (c): ContactPrimitives => ({
+          phone: c.phone,
+          name: c.name,
+        })
+      ),
+    })
+
+  private static userToDatabase = (user: UserPrimitives): UsersTable => ({
+    id: user.id,
+    name: user.name,
+    phone: user.phone,
+    last_name: user.lastName,
+  })
+
+  private static contactToDatabase =
+    (userId: string) =>
+    (contact: ContactPrimitives): ContactsTable => {
+      return {
+        name: contact.name,
+        phone: contact.phone,
+        user_id: userId,
+      }
+    }
+
   constructor(@Inject(DATABASE_CONNECTION) private db: DatabaseConnection) {}
 
   async findById(userId: UserId): Promise<Nullable<User>> {
     const user = await this.db
       .selectFrom('users')
       .selectAll()
+      .select(
+        sql<ContactsTable[]>`(
+          select coalesce(json_agg(contacts), '[]'::json) 
+          from contacts
+          where contacts.user_id = users.id
+        )`.as('contacts')
+      )
       .where('users.id', '=', userId.toPrimitives())
       .executeTakeFirst()
 
     if (!user) return null
 
-    return this.populateAggregateEntities(user)
+    return UserRepositoryKysely.toDomain(user)
   }
 
   async findByPhone(phone: UserPhoneNumber): Promise<Nullable<User>> {
     const user = await this.db
       .selectFrom('users')
       .selectAll()
+      .select(
+        sql<ContactsTable[]>`(
+          select coalesce(json_agg(contacts), '[]'::json) 
+          from contacts
+          where contacts.user_id = users.id
+        )`.as('contacts')
+      )
       .where('users.phone', '=', phone.toPrimitives())
       .executeTakeFirst()
 
     if (!user) return null
 
-    return this.populateAggregateEntities(user)
+    return UserRepositoryKysely.toDomain(user)
   }
 
   async isUser(contact: UserContact): Promise<boolean> {
@@ -53,61 +101,29 @@ export class UserRepositoryKysely implements UserRepository {
 
     await this.db
       .insertInto('users')
-      .values({
-        id: userPrimitives.id,
-        name: userPrimitives.name,
-        phone: userPrimitives.phone,
-        last_name: userPrimitives.lastName,
-      })
+      .values(UserRepositoryKysely.userToDatabase(userPrimitives))
       .onConflict((oc) =>
-        oc.column('id').doUpdateSet({
-          name: userPrimitives.name,
-          phone: userPrimitives.phone,
-          last_name: userPrimitives.lastName,
-        })
+        oc.column('id').doUpdateSet(UserRepositoryKysely.userToDatabase(userPrimitives))
       )
       .execute()
 
-    await this.db.deleteFrom('contacts').where('contacts.user_id', '=', userPrimitives.id).execute()
+    await this.saveContacts(userPrimitives)
+  }
 
-    if (userPrimitives.contacts.length) {
-      await this.db
-        .insertInto('contacts')
-        .values(
-          userPrimitives.contacts.map(
-            (contact): ContactsTable => ({
-              name: contact.name,
-              phone: contact.phone,
-              user_id: userPrimitives.id,
-            })
-          )
-        )
-        .execute()
+  private async saveContacts({ id, contacts }: UserPrimitives) {
+    await this.db.deleteFrom('contacts').where('contacts.user_id', '=', id).execute()
+
+    if (!contacts.length) {
+      return
     }
+
+    await this.db
+      .insertInto('contacts')
+      .values(contacts.map(UserRepositoryKysely.contactToDatabase(id)))
+      .execute()
   }
 
   async destroy() {
     await this.db.destroy()
-  }
-
-  private async populateAggregateEntities(user: UsersTable) {
-    const contacts = await this.db
-      .selectFrom('contacts')
-      .selectAll()
-      .where('contacts.user_id', '=', user.id)
-      .execute()
-
-    return User.fromPrimitives({
-      id: user.id,
-      phone: user.phone,
-      name: user.phone,
-      lastName: user.last_name,
-      contacts: contacts.map(
-        (c): ContactPrimitives => ({
-          phone: c.phone,
-          name: c.name,
-        })
-      ),
-    })
   }
 }
