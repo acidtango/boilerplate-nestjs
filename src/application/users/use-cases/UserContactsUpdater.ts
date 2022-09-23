3cd63d243d0027c07ef84d542b67f8012dd29b13
albertogonzalezacid <albertogonzalez@acidtango.com>
2022-09-23 14:22:41 +0100

    ref: updated usecase method

--- src/application/users/use-cases/UserContactsUpdater.ts
+++ src/application/users/use-cases/UserContactsUpdater.ts
@@ -19,6 +19,6 @@ export class UserContactsUpdater extends UseCase {
     const user = await this.userFinder.findById(userId)
 
     user.updateContacts(contacts)
-    await this.userRepository.update(user)
+    await this.userRepository.save(user)
   }
 }
