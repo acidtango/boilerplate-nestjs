d7d02a4fa457f151190e841d273d609ada5a6b93
Alberto González <albertogonzalez@acidtango.com>
2023-04-18 20:46:49 +0100

    fix: added usecase

--- src/application/talks/TalksModule.ts
+++ src/application/talks/TalksModule.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { CreateTalkEndpoint } from './infrastructure/controllers/CreateTalkEndpoint'
+import { CreateTalkUseCase } from './use-cases/CreateTalkUsecase'
 
 @Module({
   controllers: [CreateTalkEndpoint],
+  providers: [CreateTalkUseCase],
 })
 export class TalksModule {}
