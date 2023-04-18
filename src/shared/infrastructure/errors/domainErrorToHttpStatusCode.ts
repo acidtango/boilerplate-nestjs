2bfd175f6b21e6dd4ef91d2e96d1bbe03dd6e397
Daniel Ramos <danielramosacosta1@gmail.com>
2023-04-18 20:04:32 +0100

    ensure speaker is not over age

--- src/shared/infrastructure/errors/domainErrorToHttpStatusCode.ts
+++ src/shared/infrastructure/errors/domainErrorToHttpStatusCode.ts
@@ -5,4 +5,5 @@ export const domainErrorToHttpStatusCode: Record<DomainErrorCode, number> = {
   [DomainErrorCode.AGGREGATE_ALREADY_CREATED_ERROR]: HttpStatus.CONFLICT,
   [DomainErrorCode.INVALID_DATE_RANGE]: HttpStatus.BAD_REQUEST,
   [DomainErrorCode.SPEAKER_DOES_NOT_EXISTS]: HttpStatus.NOT_FOUND,
+  [DomainErrorCode.UNDERAGE_SPEAKER]: HttpStatus.BAD_REQUEST,
 }
