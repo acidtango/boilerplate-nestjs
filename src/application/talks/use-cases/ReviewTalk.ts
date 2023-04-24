f397843e14f8503756096500abfd6b7fff4e60ac
Daniel Ramos <danielramosacosta1@gmail.com>
2023-04-24 15:37:16 +0100

    inject repository

--- src/application/talks/use-cases/ReviewTalk.ts
+++ src/application/talks/use-cases/ReviewTalk.ts
@@ -3,14 +3,19 @@ import { OrganizerId } from '../../../shared/domain/ids/OrganizerId'
 import { TalkId } from '../../../shared/domain/ids/TalkId'
 import { TalkRepository } from '../domain/TalkRepository'
 import { TalkNotFoundError } from '../domain/errors/TalkNotFoundError'
+import { Inject, Injectable } from '@nestjs/common'
+import { AppProvider } from '../../AppProviders'
 
 export type CreateTalkParams = {
   talkId: TalkId
   reviewerId: OrganizerId
 }
 
+@Injectable()
 export class ReviewTalk extends UseCase {
-  constructor(private readonly talkRepository: TalkRepository) {
+  constructor(
+    @Inject(AppProvider.TALK_REPOSITORY) private readonly talkRepository: TalkRepository
+  ) {
     super()
   }
 
