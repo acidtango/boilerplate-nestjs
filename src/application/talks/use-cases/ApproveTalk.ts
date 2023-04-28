5ac60b45bb049316a3eae3e6cc9d0cd157b02e41
Daniel Ramos <danielramosacosta1@gmail.com>
2023-04-28 17:39:04 +0200

    remove TalkFinder domain service

--- src/application/talks/use-cases/ApproveTalk.ts
+++ src/application/talks/use-cases/ApproveTalk.ts
@@ -2,24 +2,25 @@ import { Inject, Injectable } from '@nestjs/common'
 import { UseCase } from '../../../shared/domain/hex/UseCase'
 import { AppProvider } from '../../AppProviders'
 import { TalkRepository } from '../domain/TalkRepository'
-import { TalkFinder } from '../domain/TalkFinder'
 import { TalkStatus } from '../domain/TalkStatus'
 import { TalkCannotBeApprovedError } from '../domain/errors/TalkCannotBeApprovedError'
 import { Talk } from '../domain/Talk'
+import { TalkNotFoundError } from '../domain/errors/TalkNotFoundError'
 
 @Injectable()
 export class ApproveTalk extends UseCase {
-  private readonly talkFinder: TalkFinder
-
   constructor(
     @Inject(AppProvider.TALK_REPOSITORY) private readonly talkRepository: TalkRepository
   ) {
     super()
-    this.talkFinder = new TalkFinder(talkRepository)
   }
 
   async execute(talkId: string) {
-    const talk = await this.talkFinder.findOrThrow(talkId)
+    const talk = await this.talkRepository.findBy(talkId)
+
+    if (!talk) {
+      throw new TalkNotFoundError(talkId)
+    }
 
     if (this.getCurrentStatus(talk) === TalkStatus.PROPOSAL) throw new TalkCannotBeApprovedError()
 
