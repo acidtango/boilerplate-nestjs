9e23a2e43a3d9cb1243520f26f929bb3d8baa13b
Daniel Ramos <danielramosacosta1@gmail.com>
2023-04-28 17:33:41 +0200

    remove talk#getCurrentStatus method

--- src/application/talks/infrastructure/controllers/GetTalkEndpoint.ts
+++ src/application/talks/infrastructure/controllers/GetTalkEndpoint.ts
@@ -2,6 +2,8 @@ import { Controller, Get, HttpStatus, Param } from '@nestjs/common'
 import { DocumentationTag, Endpoint } from '../../../../utils/decorators/Endpoint'
 import { GetTalk } from '../../use-cases/GetTalk'
 import { TalkResponseDTO } from './dtos/TalkResponseDTO'
+import { Talk } from '../../domain/Talk'
+import { TalkStatus } from '../../domain/TalkStatus'
 
 @Controller('/v1/talks/:id')
 export class GetTalkEndpoint {
@@ -16,6 +18,22 @@ export class GetTalkEndpoint {
   async execute(@Param('id') id: string): Promise<TalkResponseDTO> {
     const talk = await this.getTalk.execute(id)
 
-    return TalkResponseDTO.create(talk.toPrimitives())
+    const params = talk.toPrimitives()
+    return TalkResponseDTO.create({
+      ...params,
+      status: this.getCurrentStatus(talk),
+    })
+  }
+
+  private getCurrentStatus(talk: Talk) {
+    if (talk.getIsApproved()) {
+      return TalkStatus.APPROVED
+    } else if (talk.getIsApproved() === false) {
+      return TalkStatus.REJECTED
+    } else if (talk.getReviewerId()) {
+      return TalkStatus.REVIEWING
+    } else {
+      return TalkStatus.PROPOSAL
+    }
   }
 }
