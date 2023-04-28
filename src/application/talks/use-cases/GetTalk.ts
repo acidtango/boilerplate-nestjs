5096e909f48935e6f07aba8c28d616ddd967334f
Daniel Ramos <danielramosacosta1@gmail.com>
2023-04-28 17:39:04 +0200

    remove TalkFinder domain service

--- src/application/talks/use-cases/GetTalk.ts
+++ src/application/talks/use-cases/GetTalk.ts
@@ -3,18 +3,23 @@ import { Talk } from '../domain/Talk'
 import { TalkRepository } from '../domain/TalkRepository'
 import { Inject, Injectable } from '@nestjs/common'
 import { AppProvider } from '../../AppProviders'
-import { TalkFinder } from '../domain/TalkFinder'
+import { TalkNotFoundError } from '../domain/errors/TalkNotFoundError'
 
 @Injectable()
 export class GetTalk extends UseCase {
-  private readonly talkFinder: TalkFinder
-
-  constructor(@Inject(AppProvider.TALK_REPOSITORY) talkRepository: TalkRepository) {
+  constructor(
+    @Inject(AppProvider.TALK_REPOSITORY) private readonly talkRepository: TalkRepository
+  ) {
     super()
-    this.talkFinder = new TalkFinder(talkRepository)
   }
 
   async execute(talkId: string): Promise<Talk> {
-    return this.talkFinder.findOrThrow(talkId)
+    const talk = await this.talkRepository.findBy(talkId)
+
+    if (!talk) {
+      throw new TalkNotFoundError(talkId)
+    }
+
+    return talk
   }
 }
--- src/application/talks/use-cases/ReviewTalk.ts
+++ src/application/talks/use-cases/ReviewTalk.ts
@@ -2,12 +2,12 @@ import { Inject, Injectable } from '@nestjs/common'
 import { EventBus } from '../../../shared/domain/hex/EventBus'
 import { UseCase } from '../../../shared/domain/hex/UseCase'
 import { AppProvider } from '../../AppProviders'
-import { TalkFinder } from '../domain/TalkFinder'
 import { TalkRepository } from '../domain/TalkRepository'
 import { TalkStatus } from '../domain/TalkStatus'
 import { TalkAlreadyBeingReviewed } from '../domain/errors/TalkAlreadyBeingReviewed'
 import { TalkAssignedForReview } from '../domain/TalkAssignedForReview'
 import { Talk } from '../domain/Talk'
+import { TalkNotFoundError } from '../domain/errors/TalkNotFoundError'
 
 export type ReviewTalkParams = {
   talkId: string
@@ -16,18 +16,19 @@ export type ReviewTalkParams = {
 
 @Injectable()
 export class ReviewTalk extends UseCase {
-  private readonly talkFinder: TalkFinder
-
   constructor(
     @Inject(AppProvider.EVENT_BUS) private readonly eventBus: EventBus,
     @Inject(AppProvider.TALK_REPOSITORY) private readonly talkRepository: TalkRepository
   ) {
     super()
-    this.talkFinder = new TalkFinder(talkRepository)
   }
 
   async execute({ talkId, reviewerId }: ReviewTalkParams) {
-    const talk = await this.talkFinder.findOrThrow(talkId)
+    const talk = await this.talkRepository.findBy(talkId)
+
+    if (!talk) {
+      throw new TalkNotFoundError(talkId)
+    }
 
     if (this.getCurrentStatus(talk) === TalkStatus.REVIEWING) {
       throw new TalkAlreadyBeingReviewed(talk.getTalkId())
