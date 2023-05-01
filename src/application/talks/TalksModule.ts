905d2a1abdd3bb49413391a9f2fb665ae4311854
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-01 12:58:14 +0100

    move logic from ReviewTalk to endpoint

--- src/application/talks/TalksModule.ts
+++ src/application/talks/TalksModule.ts
@@ -5,13 +5,12 @@ import { GetTalkEndpoint } from './infrastructure/controllers/GetTalkEndpoint'
 import { ReviewTalkEndpoint } from './infrastructure/controllers/ReviewTalkEndpoint'
 import { CreateTalk } from './use-cases/CreateTalk'
 import { GetTalk } from './use-cases/GetTalk'
-import { ReviewTalk } from './use-cases/ReviewTalk'
 import { ApproveTalk } from './use-cases/ApproveTalk'
 import { TalkRepositoryModule } from './infrastructure/repositories/TalkRepositoryModule'
 
 @Module({
   imports: [TalkRepositoryModule],
   controllers: [CreateTalkEndpoint, GetTalkEndpoint, ReviewTalkEndpoint, ApproveTalkEndpoint],
-  providers: [CreateTalk, GetTalk, ReviewTalk, ApproveTalk],
+  providers: [CreateTalk, GetTalk, ApproveTalk],
 })
 export class TalksModule {}
