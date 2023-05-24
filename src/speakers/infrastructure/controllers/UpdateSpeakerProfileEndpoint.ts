db53cd09ad1cbb5346a55cc25fcd71d1c3e9b51a
Daniel Ramos <danielramosacosta1@gmail.com>
2023-05-24 01:28:15 +0100

    add endpoint documentation (#184)

--- src/speakers/infrastructure/controllers/UpdateSpeakerProfileEndpoint.ts
+++ src/speakers/infrastructure/controllers/UpdateSpeakerProfileEndpoint.ts
@@ -5,6 +5,8 @@ import { UpdateSpeakerProfile } from '../../use-cases/UpdateSpeakerProfile'
 import { SpeakerId } from '../../../shared/domain/models/ids/SpeakerId'
 import { SpeakerName } from '../../domain/models/SpeakerName'
 import { SpeakerAge } from '../../domain/models/SpeakerAge'
+import { ApiParam } from '@nestjs/swagger'
+import { CONCHA_ASENSIO } from '../../../shared/infrastructure/fixtures/speakers'
 
 @Controller('/v1/speakers/:id/profile')
 export class UpdateSpeakerProfileEndpoint {
@@ -15,6 +17,7 @@ export class UpdateSpeakerProfileEndpoint {
     description: 'Updates a speaker profile',
     status: HttpStatus.OK,
   })
+  @ApiParam({ name: 'id', example: CONCHA_ASENSIO.id })
   @Put()
   async execute(@Param('id') id: string, @Body() body: SpeakerProfileDTO) {
     await this.updateSpeakerProfile.execute({
--- src/talks/infrastructure/controllers/ApproveTalkEndpoint.ts
+++ src/talks/infrastructure/controllers/ApproveTalkEndpoint.ts
@@ -2,6 +2,8 @@ import { Controller, HttpStatus, Param, Put } from '@nestjs/common'
 import { DocumentationTag, Endpoint } from '../../../shared/infrastructure/decorators/Endpoint'
 import { ApproveTalk } from '../../use-cases/ApproveTalk'
 import { TalkId } from '../../../shared/domain/models/ids/TalkId'
+import { ApiParam } from '@nestjs/swagger'
+import { JUNIOR_XP } from '../../../shared/infrastructure/fixtures/talks'
 
 @Controller('/v1/talks/:id/approve')
 export class ApproveTalkEndpoint {
@@ -12,6 +14,7 @@ export class ApproveTalkEndpoint {
     description: 'Approve talk',
     status: HttpStatus.OK,
   })
+  @ApiParam({ name: 'id', example: JUNIOR_XP.id })
   @Put()
   async execute(@Param('id') id: string) {
     await this.approveTalk.execute(TalkId.fromPrimitives(id))
