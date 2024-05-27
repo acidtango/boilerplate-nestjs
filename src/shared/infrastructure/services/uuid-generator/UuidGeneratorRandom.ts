69599001f64a1f5f5981ce1e44027734ea5c8fc3
Daniel Ramos <danielramosacosta1@gmail.com>
2024-05-27 09:13:33 +0200

    format

--- src/shared/infrastructure/services/uuid-generator/UuidGeneratorRandom.ts
+++ src/shared/infrastructure/services/uuid-generator/UuidGeneratorRandom.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "node:crypto"
+import { randomUUID } from 'node:crypto'
 import { UuidGenerator } from '../../../domain/services/UuidGenerator'
 
 export class UuidGeneratorRandom implements UuidGenerator {
